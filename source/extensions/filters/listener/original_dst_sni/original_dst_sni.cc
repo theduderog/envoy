a09d0dea1c9571e193a1b4052261feb9a61c292e
Roger Hoover <roger.hoover@gmail.com>
2019-07-31 11:29:30 -0700

    Clean up code into functions

--- source/extensions/filters/listener/original_dst_sni/original_dst_sni.cc
+++ source/extensions/filters/listener/original_dst_sni/original_dst_sni.cc
@@ -1,45 +1,76 @@
 #include "extensions/filters/listener/original_dst_sni/original_dst_sni.h"
 
+#include <regex>
+#include <sstream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "envoy/network/listen_socket.h"
 
+#include "common/network/address_impl.h"
 #include "common/common/assert.h"
 #include "common/network/utility.h"
+#include "common/singleton/const_singleton.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace ListenerFilters {
 namespace OriginalDstSni {
 
-Network::Address::InstanceConstSharedPtr OriginalDstSniFilter::getOriginalDst(int fd) {
-  return Network::Utility::getOriginalDst(fd);
-}
+class ConstantValues {
+public:
+  const std::regex NODE_PATTERN{"^[a-z0-9]{4}\\.", std::regex::optimize};
+  const u_int16_t PORT = 9092; //TODO - get from config
+};
+using Constants = ConstSingleton<ConstantValues>;
 
 Network::FilterStatus OriginalDstSniFilter::onAccept(Network::ListenerFilterCallbacks& cb) {
   ENVOY_LOG(debug, "original_dst_sni: New connection accepted");
   Network::ConnectionSocket& socket = cb.socket();
 
-  auto serverName = socket.requestedServerName();
-  if (!serverName.empty()) {
-    //ENVOY_LOG(debug, serverName);
-    auto addr = Network::Utility::parseInternetAddressAndPort("54.213.74.14:9092", false);
-    socket.restoreLocalAddress(addr);
+  //TODO - get this via config
+  u_int32_t first_2_octects;
+  sscanf("36d5", "%x", &first_2_octects);
+
+  //TLS inspector must be configured first so that this is set from SNI
+  auto server_name = socket.requestedServerName();
+  if (!server_name.empty()) {
+    // Check if server name matches broker pattern
+    if (matchesNodeNamePattern(server_name)) {
+      auto addr = getNodeAddress(first_2_octects, server_name);
+      //Set this as "local address" so the the original destination LB will use it
+      socket.restoreLocalAddress(addr);
+    }
   }
 
-  // if (local_address.type() == Network::Address::Type::Ip) {
-  //   Network::Address::InstanceConstSharedPtr original_local_address =
-  //       getOriginalDst(socket.ioHandle().fd());
-
-  //   // A listener that has the use_original_dst flag set to true can still receive
-  //   // connections that are NOT redirected using iptables. If a connection was not redirected,
-  //   // the address returned by getOriginalDst() matches the local address of the new socket.
-  //   // In this case the listener handles the connection directly and does not hand it off.
-  //   if (original_local_address) {
-  //     // Restore the local address to the original one.
-  //     socket.restoreLocalAddress(original_local_address);
-  //   }
-  // }
-
   return Network::FilterStatus::Continue;
+}
+
+bool OriginalDstSniFilter::matchesNodeNamePattern(absl::string_view server_name) {
+  return std::regex_search(server_name.begin(), server_name.end(), Constants::get().NODE_PATTERN);
+}
+
+Envoy::Network::Address::InstanceConstSharedPtr OriginalDstSniFilter::getNodeAddress(u_int32_t first_2_octets, absl::string_view server_name) {
+  auto hex_str = server_name.substr(0, 4);
+  auto last_2_octets = parseHexOctet(hex_str);
+  return makeAddress(first_2_octets, last_2_octets);
+}
+
+u_int32_t OriginalDstSniFilter::parseHexOctet(absl::string_view hex_str) {
+  std::stringstream ss;
+  ss << std::hex << hex_str;
+  u_int32_t result;
+  ss >> result;
+  return result;
+}
+
+Envoy::Network::Address::InstanceConstSharedPtr OriginalDstSniFilter::makeAddress(u_int32_t first_2_octets, u_int32_t last_2_octets) {
+  in_addr_t iaddr = (htonl(first_2_octets) >> 16) | htonl(last_2_octets);
+  sockaddr_in sa4;
+  sa4.sin_addr = in_addr{iaddr};
+  sa4.sin_family = AF_INET;
+  sa4.sin_port = htons(Constants::get().PORT);
+  return std::make_shared<Envoy::Network::Address::Ipv4Instance>(&sa4);
 }
 
 } // namespace OriginalDstSni
--- source/extensions/filters/listener/original_dst_sni/original_dst_sni.h
+++ source/extensions/filters/listener/original_dst_sni/original_dst_sni.h
@@ -14,10 +14,16 @@ namespace OriginalDstSni {
  */
 class OriginalDstSniFilter : public Network::ListenerFilter, Logger::Loggable<Logger::Id::filter> {
 public:
-  virtual Network::Address::InstanceConstSharedPtr getOriginalDst(int fd);
+  //virtual Network::Address::InstanceConstSharedPtr getOriginalDst(int fd);
 
   // Network::ListenerFilter
   Network::FilterStatus onAccept(Network::ListenerFilterCallbacks& cb) override;
+
+private:
+  bool matchesNodeNamePattern(absl::string_view server_name);
+  Envoy::Network::Address::InstanceConstSharedPtr getNodeAddress(u_int32_t first_2_octets, absl::string_view server_name);
+  u_int32_t parseHexOctet(absl::string_view hex_str);
+  Envoy::Network::Address::InstanceConstSharedPtr makeAddress(u_int32_t first_2_octets, u_int32_t last_2_octets);
 };
 
 } // namespace OriginalDstSni
